483d47897a83c90b036f743d622b877f57872e25
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-02-01 15:02:37 +0800

    can reading chunk configure from file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <iostream>
+#include <fstream>
 #include "LatticeChunk.h"
 #include "NewTypDefinition.h"
 
+void readFile(ChunkMap* map) {
+  std::string lineText;
+
+  std::ifstream readFile("chunk.txt");
+  int index = 0;
+  while (getline(readFile, lineText)) {
+    for (int i = 0; i < lineText.length(); i++) {
+      map->at(index) = lineText.at(i);
+      index++;
+    }
+  }
+  readFile.close();
+}
+
 int main() {
   ChunkMap map{};
-  LatticeChunk * latticeChunk = new LatticeChunk(map);
+  readFile(&map);
   
+  //test
+
+  LatticeChunk * latticeChunk = new LatticeChunk(map);
+
   return 0;
 }
